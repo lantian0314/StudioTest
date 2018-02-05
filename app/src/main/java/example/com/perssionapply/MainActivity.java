bc320f1b19bd89680d8e5ce9888386e16be759f3
lantian0314 <384289339@qq.com>
2018-02-05 17:38:20 +0800

    测试提交

--- app/src/main/java/example/com/perssionapply/MainActivity.java
+++ app/src/main/java/example/com/perssionapply/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        String string="ceshi";
     }
 
     @Override
